4ace6f8505c822eea3975c16f28924156114013f
thomasevans <thomas.jude.evans@gmail.com>
2016-03-21 16:17:48 +0100

    Needs more debugging, but saving in progress version

--- flight_truncation.R
+++ flight_truncation.R
@@ -5,19 +5,160 @@
 # Load in data -----
 
 # GPS point data
+load("points.detailed.RData")
+
 
 
 # Flight summary data
+load("flights.RData")
+
+# 
+# points.info <- merge(points.detailed, flights, by = "flight_id_combined")
+# 
+# 
+# 
+# test.df <- points.info[points.info$species == "murre" & points.info$device_type == "igu",]
+# test.df.x <- unique(test.df)
 
 
+
+points.detailed$point_id <- 1:nrow(points.detailed)
 
 # Truncation thing (find first point to include) --------
 
+# Code to calculate distances
+source("deg.dist.R")
+
+# Number of flights
+n_flights <- nrow(flights)
+
+# i <- 45
 
 
-# Final point to include (island distance/ colony distance) -------
+i <- c(1:n_flights)[flights$flight_id_combined == "m700"]
+
+flight.details <- list()
+points.included <- list()
+
+# For each flight do:
+for(i in 1:n_flights){
+  # for(i in 1:10){
+    
+  # Subset GPS data
+  pointsx <- points.detailed[points.detailed$flight_id_combined == flights$flight_id_combined[i],]
+  
+  # Number of GPS locations
+  n <- nrow(pointsx)
+  
+  # Make an index of GPS points
+  points.index <- 1:n
+  
+
+  
+  # Distance from island centre
+  karlso.cen.long   <-  17.972088
+  karlso.cen.lat    <-  57.284804
+  
+  island.dist <- deg.dist(karlso.cen.long, karlso.cen.lat,
+                          pointsx$longitude, pointsx$latitude,
+                          km = FALSE)
+  # plot(island.dist)
+  # plot(pointsx$latitude~pointsx$longitude)
+  
+  # Find first point < 2 km from island
+  island.buffer <- island.dist <2000
+  if(sum(island.buffer) >=1){
+    last.point <- min(points.index[island.buffer])
+  } else {last.point <- max(points.index)}
+  
+  
+  
+  # Time interval between GPS locations
+  time_interval <- as.numeric(difftime(pointsx$date_time[2:n],pointsx$date_time[1:n-1], units = "secs"))
+  time_interval <- c(0, time_interval)
+  
+  # Velocity relative to island centre
+  d.dist <- island.dist[2:n] - island.dist[1:(n-1)]
+  d.dist <- c(0, d.dist)
+  
+  d.speed <- d.dist/ time_interval
+  # plot(d.speed)
+  
+  #Change in speed from previous points
+  d.dif <- function(ia, ds = ds){
+    mean(ds[(ia ):(ia + 2)]) / mean(ds[(ia - 1):(ia - 3)])
+  }
+  
+  thresh <- 0.3
+  
+  if((last.point)>5){
+      x <- rev(sapply(c(3:(length(d.speed[last.point:1])-2)), d.dif, ds = d.speed[last.point:1]))
+      x <- c(x,1,1,1,1)
+      length(x) == n
+      z <- x>0.3
+      
+      z[is.na(z)] <- TRUE
+      first.point <- max((1:length(z))[!z])
+      if(is.infinite(first.point)){first.point <- 1
+      }
+    }else {first.point <- 1}
+  
+  
+  points2include <- c(first.point:last.point)
+  np <- length(points2include)
+  
+  flight.info <- cbind.data.frame(pointsx$flight_id_combined[1],
+                                  pointsx$date_time[points2include[1]],
+                                  pointsx$date_time[points2include[np]])
+  label.points <- rep(FALSE,n)
+  label.points[points2include] <- TRUE
+  
+  flight.details[[i]] <- flight.info
+  
+  points.included[[i]] <- cbind.data.frame(pointsx$date_time,
+                                           pointsx$device_info_serial,
+                                           pointsx$point_id,
+                                           label.points,
+                                           d.dist)
+  
+#   plot(pointsx$latitude~pointsx$longitude)
+#   points(pointsx$latitude[!label.points]~pointsx$longitude[!label.points],
+#          col = "red")
+  
+}
 
 
+flights.details.df <- do.call(rbind , flight.details)
+names(flights.details.df) <- c("flight_id_combined", "date_time_include_start",
+                               "date_time_include_end")
+
+points.included.df <- do.call(rbind , points.included)
+names(points.included.df) <- c("date_time", "device_info_serial",
+                               "point_id", "included_points",
+                               "dist_sk_centre_m")
+# Are all flights represented in the table?
+all(flights$flight_id_combined %in% as.character(flights.details.df$flight_id_combined))
+all(points.detailed$point_id %in% points.included.df$point_id)
+
+summary(points.included.df$included_points)
+
+summary(points.included.df$date_time == points.detailed$date_time)
+
+
+# Merge with current tables -------
+str(points.included.df)
+str(points.detailed)
+
+points.included.df$device_info_serial <- as.character(points.included.df$device_info_serial)
+
+points.df <- merge(points.detailed, points.included.df,
+                   by = "point_id")
+
+points.df <- points.df[order(c(points.df$device_info_serial,points.df$date_time)),]
+
+# points.df2 <- points.df[!is.na(points.df$date_time),]
+
+# ?merge
 
 # Output the data -----
 
