c375302f2dfa2d4f9fb7a9c1ef574b1c7ec57b42
thomasevans <thomas.jude.evans@gmail.com>
2016-03-21 10:05:17 +0100

    Initiation of new script to truncate flights to retain only directed portion of flight and remove final points near colony

---
+++ flight_truncation.R
@@ -0,0 +1,27 @@
+# Script to truncate flights to remove non-directed initial parts of flights
+# and to remove final parts of flights when birds approach island
+
+
+# Load in data -----
+
+# GPS point data
+
+
+# Flight summary data
+
+
+
+# Truncation thing (find first point to include) --------
+
+
+
+# Final point to include (island distance/ colony distance) -------
+
+
+
+# Output the data -----
+
+# Points data
+
+
+# Flight summary data
