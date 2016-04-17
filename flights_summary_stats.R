ec172f1f1aebf9b8615145c628f6d096a7431d41
thomasevans <thomas.jude.evans@gmail.com>
2016-04-17 11:41:59 +0200

    things to include (comments)

--- flights_summary_stats.R
+++ flights_summary_stats.R
@@ -1,3 +1,28 @@
 # Calculate summary statistics for each flight for statistical analysis and general
 # summary
 
+# Get data -----
+
+# Flight truncation information
+load("points.detailed.incl.RData")
+
+
+# GPS locations + calculated variables
+
+
+
+# Calculate/ assemble general summary data ----
+# e.g. distance travelled, bird_id, etc.
+
+
+# Mean/ median for wind conditions, and calculated wind dependent information
+# e.g. alpha...
+
+
+# Get airspeed (probably median??)
+
+
+# Get altitude (include quality criteria in this selection)
+
+
+# Output to new table ----
