fc88b6d83ea81c3d162112398413fe74ac81d3b7
thomasevans <thomas.jude.evans@gmail.com>
2016-04-20 12:06:46 +0200

    Initiate script for methods illustration figure showing how different components were extracted for flights

---
+++ flight_illustration_methods.R
@@ -0,0 +1,23 @@
+# Example figure for methods of flight analysis
+
+
+# Load in data -------
+
+# Points
+
+
+# Summary data
+
+
+
+# **** Figures -----
+
+
+# Flight map ------
+# Points during flight:
+# Colour - rainbow colour by time - with alpha channel (lighter for excluded??)
+# Size - by altitude
+# Lines between points (colour by included - black, excluded - red 
+# according to truncation thing)
+# Mark example point representing aproximately a median point/ 
+# representative location
