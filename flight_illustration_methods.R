405fdd6c9b98f325658dddcd20f4a9dabb5a4bb3
thomasevans <thomas.jude.evans@gmail.com>
2016-04-20 15:41:47 +0200

    Planning figures (just comments)

--- flight_illustration_methods.R
+++ flight_illustration_methods.R
@@ -13,11 +13,62 @@
 # **** Figures -----
 
 
-# Flight map ------
+# 1. Flight map ------
+# Base map with land on
+
 # Points during flight:
 # Colour - rainbow colour by time - with alpha channel (lighter for excluded??)
 # Size - by altitude
-# Lines between points (colour by included - black, excluded - red 
-# according to truncation thing)
 # Mark example point representing aproximately a median point/ 
-# representative location
+# representative location - maybe an X
+
+# Circle showing distance buffer around island, with centre shown
+
+# Lines between points
+# colour by included - black, excluded - red according to truncation thing
+
+
+# 2. Multi-panel figure of components over time -----
+
+# For all vertical lines to indicate section of flight included
+# No x-axis for A + B, but common range as for C, so can use common X-axis
+
+# Panel A - Height (GPS + calibrated)
+# - horizontal line to indicate median value during flight
+
+# Panel B - Vg (GPS speed) over time
+# - common time axis
+# - horizontal line to indicate median value during flight
+
+# Panel C -  distance from island over time
+# - plot distance over time
+
+
+# 3. Wind shear illustration ------
+# Altitude vs. wind-speed - for example track
+
+# Wind speed on x-axis (so that altitude is on y-axis)
+
+# Height on y-axis, with range 0-100 m
+
+# Indicate 10 m height, with dashed lines - for reference altitude
+
+# Indicate actual flight altitude and calculated wind-speed for that point
+# dashed lines - as for reference altitude
+
+
+# 4. Velocity components ----
+# similar to figure 2 in Tarroux et al.
+# Could use common notation, for simplicity etc.
+
+# Do for same example flight as all above.
+
+
+
+# **** Output figure -----
+
+# Could either combine all, using layout etc, or print 4 panels sepperatly then combine
+# with another program, e.g. illustrator, GIMP etc.
+# Best option is probably 4 sepperatly, then can combine to single pdf using
+# Inkscape, following workflow described here
+
