0da0859913f555e775444c80758e407335ec9899
thomasevans <thomas.jude.evans@gmail.com>
2016-04-24 21:58:45 +0200

    Plan for methods illustration figure

---
+++ gps_trip_flight_segmentation_illustration.R
@@ -0,0 +1,34 @@
+
+
+# Load in data from database ----
+# GPS points with trip_id etc.
+# For device_info_serial #803
+# Period: 2013-07-14 to 2013-07-17 (whole days)
+
+
+
+# Map all locations ----
+# All locations, no colour (black)
+
+
+# Map only trip locations ------
+#  with unique colours for each trip
+
+# 9 colours from http://colorbrewer2.org/
+col.trips <- c('#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999')
+
+# Map of focus trip only ----
+# Indicate flight points somehow...
+
+
+# Combine plots below into single output figure - same dimensions as single map
+# Plot of displacemnt (for trip recognition) ------
+# Colour by trip_id (using same colours as map)
+
+# Horizontal line for cut-off for trips
+
+# Plot of speed over time for focal trip ----
+# Could colour all points by rainbow thing for correspondance with map
+
+# High-light inward flight
+
