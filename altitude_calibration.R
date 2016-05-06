884fedcba66c26e618ad31170d6dc7a99ec70658
thomasevans <thomas.jude.evans@gmail.com>
2016-05-06 15:08:47 +0200

    Using common figure formats

--- altitude_calibration.R
+++ altitude_calibration.R
@@ -114,6 +114,7 @@ addalpha <- function(colors, alpha=1.0) {
 # Map GPS locations --------
 # needed to plot maps
 library(maps)
+library(raster)
 
 
 # Plot base map
@@ -127,16 +128,35 @@ c.ylim <- c(56.7, 57.8)
 
 # ?png
 png(filename = "surface_locations_map.png",
-    width = 9, height = 4, units = "in", pointsize = 8,
-    bg = "white", res = 600, family = "", restoreConsole = TRUE,
-    type = c("cairo-png"))
+    units = "in", pointsize = 8,
+    bg = "white", res = 600, restoreConsole = TRUE,
+    type = c("cairo-png"),
+    width = 18, height = 6, family = "serif")
+
+
+# Set global par
+par(ps = 14, cex = 1.5, cex.lab = 2)
+# 
+# svg("surface_locations_map.svg",
+#     width = 18, height = 6, family = "serif")
 
 
 # 3 maps side by side 
-par(mfrow=c(1,3))
+par(mfrow=c(1,3),cex=1)
+
+range.x <-  c.xlim[2]-c.xlim[1]
+range.y <-  c.ylim[2]-c.ylim[1]
+dif.co <- max(c(range.x,range.y))
+
+gadm_clip <- crop(gadm, extent(c.xlim[1]-1*(dif.co),
+                               c.xlim[2]+1*(dif.co),
+                               c.ylim[1]-1*(dif.co),
+                               c.ylim[2]+1*(dif.co)))
+
+
 
 # Plot base map
-plot(gadm, xlim = c.xlim,
+plot(gadm_clip, xlim = c.xlim,
      ylim = c.ylim, col="grey", bg = "white",
      main = "Gulls - UvA BiTs GPS")
 
@@ -152,8 +172,19 @@ box(col="black",lwd=2)
 axis(side=(1), las=1, col="black", col.axis="black")
 axis(side=(2), las=1, col="black", col.axis="black")
 
+range.x <-  c.xlim[2]-c.xlim[1]
+range.y <-  c.ylim[2]-c.ylim[1]
+dif.co <- max(c(range.x,range.y))
+
+gadm_clip <- crop(gadm, extent(c.xlim[1]-1*(dif.co),
+                               c.xlim[2]+1*(dif.co),
+                               c.ylim[1]-1*(dif.co),
+                               c.ylim[2]+1*(dif.co)))
+
+
+
 # Plot base map
-plot(gadm, xlim = c.xlim,
+plot(gadm_clip, xlim = c.xlim,
      ylim = c.ylim, col="grey", bg = "white",
      main = "Murres - UvA BiTs GPS")
 
@@ -164,7 +195,7 @@ points(points.uva.murres$longitude, points.uva.murres$latitude,
 # Scale bar and axis
 x <- c.xlim[1] + (c.xlim[2] - c.xlim[1])/20
 y <- c.ylim[1] + (c.ylim[2] - c.ylim[1])/10
-map.scale(x,y,ratio = FALSE, col="black",col.lab="black")
+map.scale(ratio = FALSE, col="black",col.lab="black")
 box(col="black",lwd=2)
 axis(side=(1), las=1, col="black", col.axis="black")
 axis(side=(2), las=1, col="black", col.axis="black")
@@ -175,13 +206,24 @@ axis(side=(2), las=1, col="black", col.axis="black")
 # Scale bar and axis
 x <- c.xlim[1] + (c.xlim[2] - c.xlim[1])/20
 y <- c.ylim[1] + (c.ylim[2] - c.ylim[1])/10
-map.scale(x,y,ratio = FALSE, col="black",col.lab="black")
+map.scale(ratio = FALSE, col="black",col.lab="black")
 box(col="black",lwd=2)
 axis(side=(1), las=1, col="black", col.axis="black")
 axis(side=(2), las=1, col="black", col.axis="black")
 
+range.x <-  c.xlim[2]-c.xlim[1]
+range.y <-  c.ylim[2]-c.ylim[1]
+dif.co <- max(c(range.x,range.y))
+
+gadm_clip <- crop(gadm, extent(c.xlim[1]-1*(dif.co),
+                               c.xlim[2]+1*(dif.co),
+                               c.ylim[1]-1*(dif.co),
+                               c.ylim[2]+1*(dif.co)))
+
+
+
 # Plot base map
-plot(gadm, xlim = c.xlim,
+plot(gadm_clip, xlim = c.xlim,
      ylim = c.ylim, col="grey", bg = "white",
      main = "Murres - IGU GPS")
 
@@ -192,7 +234,7 @@ points(points.igu$longitude, points.igu$latitude,
 # Scale bar and axis
 x <- c.xlim[1] + (c.xlim[2] - c.xlim[1])/20
 y <- c.ylim[1] + (c.ylim[2] - c.ylim[1])/10
-map.scale(x,y,ratio = FALSE, col="black",col.lab="black")
+map.scale(ratio = FALSE, col="black",col.lab="black")
 box(col="black",lwd=2)
 axis(side=(1), las=1, col="black", col.axis="black")
 axis(side=(2), las=1, col="black", col.axis="black")
@@ -221,7 +263,33 @@ murre.igu.alt.mean <- mean(points.igu$elev[points.igu$elev >= murre.igu.alt.quan
                            na.rm = TRUE)
 
 win.metafile(filename = "altitude_variation_hist.wmf", width = 9, height = 4)
-par(mfrow=c(1,3))
+
+
+
+
+
+# Set global par
+par(ps = 14, cex = 1.5, cex.lab = 2)
+
+png(filename = "altitude_variation_hist.png",
+    units = "in",
+    bg = "white", res = 600, restoreConsole = TRUE,
+    type = c("cairo-png"),
+    width = 12, height = 4, family = "serif")
+
+# 
+svg("altitude_variation_hist.svg",
+    width = 12, height = 4, family = "serif")
+
+
+# 3 maps side by side 
+par(mfrow=c(1,3),cex=1)
+
+
+
+# par(mfrow=c(1,3))
+par(mar=c(5,5,4,1))   
+# ?par
 hist(points.uva.gulls$altitude[points.uva.gulls$altitude >= -100 &
                                  points.uva.gulls$altitude <= 100], xlim= c(-50,40),
      breaks = 100,
@@ -230,6 +298,7 @@ hist(points.uva.gulls$altitude[points.uva.gulls$altitude >= -100 &
      xlab = "GPS recorded altitude (m)")
 abline(v=gull.uva.alt.quant, lty = 2, col = "dark grey")
 abline(v=gull.uva.alt.mean, lty = 2, col = "red", lwd = 2)
+par(mar=c(5,5,4,1))   
 
 hist(points.uva.murres$altitude[points.uva.murres$altitude >= -100 &
                                   points.uva.murres$altitude <= 100], xlim= c(-50,50),
@@ -239,6 +308,7 @@ hist(points.uva.murres$altitude[points.uva.murres$altitude >= -100 &
      xlab = "GPS recorded altitude (m)")
 abline(v=murre.uva.alt.quant, lty = 2, col = "dark grey")
 abline(v=murre.uva.alt.mean, lty = 2, col = "red", lwd = 2)
+par(mar=c(5,5,4,1))   
 
 hist(points.igu$elev[points.igu$elev >= -100 &
                        points.igu$elev <= 100], xlim= c(-100,100),
@@ -263,27 +333,49 @@ plot_quantile <- function(x, main = "", xlim = NULL){
   abline(v = quantiles[c(50,90,95)], lwd = 2, lty = 2, col = "dark grey")
   text(quantiles[c(50,90,95)], 5,
        paste("<",format(round(quantiles[c(50,90,95)], 1), nsmall = 1), "m"),
-       col = "grey20")
+       col = "grey20", cex = 0.8)
   text(quantiles[c(50,90,95)], 10, c("50 %", "90 %", "95 %"),
-       col = "grey20")
+       col = "grey20", cex = 0.8)
   
   perc_val <- c((c(1:99)[quantiles>5][1]), (c(1:99)[quantiles>10][1]))
   abline(h = perc_val, lwd = 2, lty = 2, col = "red")
   if(is.null(xlim)){
     text(quantiles[97],perc_val+3, paste(perc_val, "%  <", c(5,10), "m"),
-         col = "red")
+         col = "red", cex = 0.8)
   } else {
     text(xlim[2]-20,perc_val+3, paste(perc_val, "%  <", c(5,10), "m"),
-         col = "red")
+         col = "red", cex = 0.8)
   }
 
 }
 
 
+# New altitudes ----
+gull.uva.alt.0 <- points.uva.gulls$altitude - gull.uva.alt.mean
+murre.uva.alt.0 <- points.uva.murres$altitude - murre.uva.alt.mean
+murre.igu.alt.0 <- points.igu$elev - murre.igu.alt.mean
 
 
 win.metafile(filename = "altitude_precision_quantile.wmf", width = 9, height = 4)
-par(mfrow=c(1,3))
+
+# Set global par
+par(ps = 14, cex = 1.5, cex.lab = 2)
+
+png(filename = "altitude_precision_quantile.png",
+    units = "in",
+    bg = "white", res = 600, restoreConsole = TRUE,
+    type = c("cairo-png"),
+    width = 12, height = 8, family = "serif")
+
+# 
+svg("altitude_precision_quantile.svg",
+    width = 12, height = 8, family = "serif")
+
+
+# 3 maps side by side 
+par(mfrow=c(1,3),cex=1)
+
+# par(mfrow=c(1,3))
 plot_quantile(gull.uva.alt.0,
               main = "Gulls - UvA BiTs GPS")
 
@@ -295,15 +387,30 @@ plot_quantile(murre.igu.alt.0,
 dev.off()
 
 
-# New altitudes ----
-gull.uva.alt.0 <- points.uva.gulls$altitude - gull.uva.alt.mean
-murre.uva.alt.0 <- points.uva.murres$altitude - murre.uva.alt.mean
-murre.igu.alt.0 <- points.igu$elev - murre.igu.alt.mean
 
 
 # Altitude filtering ------
 
-win.metafile(filename = "altitude_precision_sat_n_ext.wmf", width = 9, height = 9)
+# Set global par
+par(ps = 14, cex = 1.5, cex.lab = 2)
+
+png(filename = "altitude_precision_sat_n_ext.png",
+    units = "in",
+    bg = "white", res = 600, restoreConsole = TRUE,
+    type = c("cairo-png"),
+    width = 12, height = 12, family = "serif")
+
+# 
+svg("altitude_precision_sat_n_ext.svg",
+    width = 12, height = 12, family = "serif")
+
+
+# 3 maps side by side 
+# par(mfrow=c(1,3),cex=1)
+
+
+
+# win.metafile(filename = "altitude_precision_sat_n_ext.wmf", width = 9, height = 9)
 
 # UvA devices
 
@@ -324,7 +431,7 @@ for(i in 1:10){
 
 gull_sat <- cbind.data.frame(sat_number,perc_val,n_loc,p_loc)
 
-par(mfrow=c(2,2))
+par(mfrow=c(2,2),cex=1)
 par(mar = c(5,5,2,5))
 plot(gull_sat$perc_val~gull_sat$sat_number,
      ylab = "Proportion of locations with error <5 m (%)",
@@ -337,7 +444,7 @@ plot(gull_sat$p_loc*100~gull_sat$sat_number,  axes=F, ylab = "", xlab = "", col 
 axis(side = 4, col = "red", las = 1, col.axis = "red")
 # ?axis
 mtext(side = 4, line = 3, 'Proportion of GPS locations retained (%)',
-      col = "red", cex = 0.66)
+      col = "red")
 
 # summary(as.factor(points.uva.gulls$satellites_used))/nrow(points.uva.gulls)
 
@@ -375,7 +482,7 @@ plot(murre_sat$p_loc*100~murre_sat$sat_number,  axes=F, ylab = "", xlab = "", co
 axis(side = 4, col = "red", las = 1, col.axis = "red")
 # ?axis
 mtext(side = 4, line = 3, 'Proportion of GPS locations retained (%)',
-      col = "red", cex = 0.66)
+      col = "red")
 
 # summary(as.factor(points.uva.murres$satellites_used))/nrow(points.uva.murres)
 
@@ -416,7 +523,7 @@ plot(murre_sat$p_loc*100~murre_sat$sat_number,  axes=F, ylab = "", xlab = "", co
 axis(side = 4, col = "red", las = 1, col.axis = "red")
 # ?axis
 mtext(side = 4, line = 3, 'Proportion of GPS locations retained (%)',
-      col = "red", cex = 0.66)
+      col = "red")
 # ?mtext
 
 
@@ -448,7 +555,7 @@ plot(murre_sat$p_loc*100~murre_sat$sat_number,  axes=F, ylab = "", xlab = "", co
 axis(side = 4, col = "red", las = 1, col.axis = "red")
 # ?axis
 mtext(side = 4, line = 3, 'Proportion of GPS locations (%)',
-      col = "red", cex = 0.66)
+      col = "red")
 
 dev.off()
 
@@ -456,7 +563,23 @@ dev.off()
 
 
 # EHPE/ EVPE ---------
-win.metafile(filename = "altitude_precision_ehpe.wmf", width = 9, height = 4)
+# win.metafile(filename = "altitude_precision_ehpe.wmf", width = 9, height = 4)
+
+
+# Set global par
+par(ps = 14, cex = 1.5, cex.lab = 2)
+
+png(filename = "altitude_precision_ehpe.png",
+    units = "in",
+    bg = "white", res = 600, restoreConsole = TRUE,
+    type = c("cairo-png"),
+    width = 12, height = 6, family = "serif")
+
+# 
+svg("altitude_precision_ehpe.svg",
+    width = 12, height = 6, family = "serif")
+
+
 
 # UvA devices
 # hist(points.uva.gulls$v_accuracy, xlim = c(0,50), breaks = 100)
@@ -482,7 +605,7 @@ for(i in 1:100){
 
 gull_sat <- cbind.data.frame(thresh,perc_val,n_loc,p_loc)
 
-par(mfrow=c(1,3))
+par(mfrow=c(1,3), cex = 1.0)
 par(mar = c(5,5,2,5))
 plot(gull_sat$perc_val~gull_sat$thresh,
      ylab = "Proportion of locations with error <5 m (%)",
@@ -495,7 +618,7 @@ plot(gull_sat$p_loc*100~gull_sat$thresh,  axes=F, ylab = "", xlab = "", col = "r
 axis(side = 4, col = "red", las = 1, col.axis = "red")
 # ?axis
 mtext(side = 4, line = 3, 'Proportion of GPS locations retained (%)',
-      col = "red", cex = 0.66)
+      col = "red")
 
 # summary(as.factor(points.uva.gulls$satellites_used))/nrow(points.uva.gulls)
 
@@ -533,7 +656,7 @@ plot(murre_sat$p_loc*100~murre_sat$thresh,  axes=F, ylab = "", xlab = "", col = 
 axis(side = 4, col = "red", las = 1, col.axis = "red")
 # ?axis
 mtext(side = 4, line = 3, 'Proportion of GPS locations retained (%)',
-      col = "red", cex = 0.66)
+      col = "red")
 
 
 
@@ -568,13 +691,29 @@ plot(murre_sat$p_loc*100~murre_sat$thresh,  axes=F, ylab = "", xlab = "", col = 
 axis(side = 4, col = "red", las = 1, col.axis = "red")
 # ?axis
 mtext(side = 4, line = 3, 'Proportion of GPS locations retained (%)',
-      col = "red", cex = 0.66)
+      col = "red")
 
 
 dev.off()
 
 # DOP -------
-win.metafile(filename = "altitude_precision_dop.wmf", width = 8, height = 5)
+# win.metafile(filename = "altitude_precision_dop.wmf", width = 8, height = 5)
+
+
+# Set global par
+par(ps = 14, cex = 1.5, cex.lab = 2)
+
+png(filename = "altitude_precision_dop.png",
+    units = "in",
+    bg = "white", res = 600, restoreConsole = TRUE,
+    type = c("cairo-png"),
+    width = 12, height = 6, family = "serif")
+
+# 
+svg("altitude_precision_dop.svg",
+    width = 12, height = 6, family = "serif")
+
+
 
 # UvA devices
 # hist(points.uva.gulls$positiondop, xlim = c(0,10), breaks = 100)
@@ -600,7 +739,7 @@ for(i in 1:91){
 
 gull_sat <- cbind.data.frame(thresh,perc_val,n_loc,p_loc)
 
-par(mfrow=c(1,2))
+par(mfrow=c(1,2), cex = 1.0)
 par(mar = c(5,5,2,5))
 plot(gull_sat$perc_val~gull_sat$thresh,
      ylab = "Proportion of locations with error <5 m (%)",
@@ -663,7 +802,7 @@ dev.off()
 f <- !is.na(points.igu$MSVs_QCN)
 par(mfrow=c(1,1))
 boxplot(abs(murre.igu.alt.0[f])~as.factor(points.igu$MSVs_QCN[f]),
-        ylim = c(0,100),
+        ylim = c(0,100)
         )
 abline(h = 5, lty = 2, lwd = 2, col = "red")
 
@@ -695,6 +834,20 @@ points.igu.msv.merge <- merge(points.igu.msv, z[,c(1,6)], by = "MSVs_QCN")
 
 library(ggplot2)
 # ?ggsave
+
+theme_new <- theme_bw(base_size = 14, base_family = "serif") +
+  theme(legend.position = c(1, 1),
+        legend.justification = c(1, 1),
+        legend.key.size =   unit(2, "lines"),
+        legend.key = element_rect(colour =NA),
+        legend.text = element_text(size = 12),
+        axis.text = element_text(size = 14),
+        axis.title = element_text(size = 14),
+        legend.text.align = 0,
+        legend.key.width = unit(3, "lines"),
+        legend.title = element_blank()
+  )
+
 ggplot(data = points.igu.msv.merge, aes(x = as.factor(MSVs_QCN), y = abs(alt_0_abs),
                                   col = as.factor(thresh_test))) +
   geom_jitter(alpha = 0.3,size = 1) +
@@ -706,9 +859,12 @@ ggplot(data = points.igu.msv.merge, aes(x = as.factor(MSVs_QCN), y = abs(alt_0_a
        x = 'MSVs_QCN',
        y = 'Altitude error (m)') +
   guides(colour=FALSE) +
+  theme_new +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 ggsave("MSVs_QCN_quality.png",
-       width = 8, height = 5, units = "in")
+       width = 10, height = 6, units = "in")
+ggsave("MSVs_QCN_quality.svg",
+       width = 10, height = 6, units = "in")
 
 
 plot_quantile(points.igu.msv.merge$alt_0_abs[points.igu.msv.merge$thresh_test])
@@ -828,7 +984,22 @@ sum(points.uva.gulls_inc_sat$satellites_used >=7)/length(points.uva.gulls_inc_sa
 # Final precision post-filtering -----
 
 win.metafile(filename = "altitude_precision_quantile_post_filter.wmf", width = 9, height = 4)
-par(mfrow=c(1,3))
+
+# Set global par
+par(ps = 14, cex = 1.5, cex.lab = 2)
+
+png(filename = "altitude_precision_quantile_post_filter.png",
+    units = "in",
+    bg = "white", res = 600, restoreConsole = TRUE,
+    type = c("cairo-png"),
+    width = 12, height = 8, family = "serif")
+
+# 
+svg("altitude_precision_quantile_post_filter.svg",
+    width = 12, height = 8, family = "serif")
+
+
+par(mfrow=c(1,3), cex = 1.0)
 plot_quantile(gull.uva.alt.0[(
   !is.na(points.uva.gulls$satellites_used) & points.uva.gulls$satellites_used >=7)|(
     is.na(points.uva.gulls$satellites_used) & points.uva.gulls$positiondop <=3
