c990c8950aa240ea352b505098c8a0a33257aefa
thomasevans <thomas.jude.evans@gmail.com>
2016-06-01 22:14:33 +0200

    New circle plot for wind relative to track

--- flight_direction_circular_distributions.R
+++ flight_direction_circular_distributions.R
@@ -61,11 +61,11 @@ track.dir.gull.circ <- track.dir.circ[gulls]
 
 par(ps = 14, cex = 1.5, cex.lab = 2)
 
-svg("circle_plots.svg",
+svg("circle_plots_new.svg",
     width = 12, height = 4, family = "serif")
 # ?cairo_ps
 # Plot base map
-par(mfrow = c(1,3), cex = 1.0)
+par(mfrow = c(1,4), cex = 1.0)
 
 
 par(mar=c(1, 1, 1, 1))   
@@ -178,6 +178,130 @@ text(x = 1.2, y = 1.2, labels = expression(Vg),
      cex = 1.2, col = cols.new[1])
 # ?legend
 
+# Directions of Vg (track) with respect to wind -------
+va.dir <- 90 - deg(atan2(flight.details$vg_v_alt_filter,
+                         flight.details$vg_u_alt_filter))
+
+# flight.details$vg_v
+# warnings()
+fun2 <- function(x){
+  if(is.na(x))return(NA) else{
+    if(x<0)return(360 + x) else{
+      return(x)
+    }
+  }
+}
+# fun2(NA)
+va.dir <- sapply(va.dir, fun2)
+
+
+range(flight.details$ecmwf_wind_10m_dir)
+
+wind.dir.range <- flight.details$ecmwf_wind_10m_dir
+wind.dir.range[flight.details$ecmwf_wind_10m_dir < 0] <- 360 + flight.details$ecmwf_wind_10m_dir[flight.details$ecmwf_wind_10m_dir < 0]
+
+wind.dif <- wind.dir.range - va.dir
+# hist(wind.dif)
+
+wind.dif.new <- sapply(wind.dif, fun2)
+# hist(wind.dif.new)
+# 
+
+wind.dif.new.circ <- circular(wind.dif.new,
+                              type = "angles",
+                              units = "degrees",
+                              template = "none"
+)
+
+
+
+
+
+plot(wind.dif.new.circ[gulls],
+     # stack = TRUE,
+     # sep = 0.015,
+     shrink = 1.4,
+     cex = 1,
+     bin = 45,
+     col = addalpha(cols.new[2], 0.3),
+     zero = pi/2,
+     tcl.text = .2,
+     type = "n",
+     rotation = "clock")
+
+points(wind.dif.new.circ[gulls],
+       # stack = TRUE,
+       # sep = 0.045,
+       shrink = 1.2,
+       cex = 1,
+       bin = 45,
+       col = addalpha(cols.new[2], 0.3),
+       zero = pi/2,
+       pch = 21,
+       # new = FALSE,
+       rotation = "clock",
+       next.points = 0.03)
+
+# ?plot.circular
+# 
+# hist(wind.dif.new[gulls], breaks = 40)
+# points((x, pch = 16, cex = 1, stack = FALSE, sep = 0.025, 
+#         shrink = 1, bins = NULL, col = NULL, next.points = NULL, 
+#         plot.info = NULL, zero = NULL, rotation = NULL, ...))
+
+# hist(wind.dif.new[!gulls], breaks = 20)
+
+points(wind.dif.new.circ[!gulls],
+       # stack = TRUE,
+       # sep = 0.045,
+       shrink = 1.2,
+       cex = 1,
+       bin = 45,
+       pch = 21,
+       col = addalpha(cols.new[1], 0.7),
+       zero = pi/2,
+       # new = FALSE,
+       rotation = "clock",
+       next.points = -0.03)
+
+x <- wind.dif.new.circ[!gulls]
+x <- x[!is.na(x)]
+lines(density.circular(x,
+                       bw = 30),
+      col = addalpha(cols.new[1], 0.7),
+      lwd = 3,
+      # lty = 2,
+      zero = pi/2,
+      # new = FALSE,
+      rotation = "clock",
+      shrink = 0.7)
+
+
+x <- wind.dif.new.circ[gulls]
+x <- x[!is.na(x)]
+lines(density.circular(x,
+                       bw = 30),
+      col = addalpha(cols.new[2], 0.7),
+      lwd = 3,
+      # lty = 2,
+      zero = pi/2,
+      # new = FALSE,
+      rotation = "clock",
+      shrink = 0.7)
+# ?lines.circular
+legend("topleft", "(e)", bty="n", cex = 1.2) 
+# legend("topright", expression(alpha), bty="n", cex = 1.2) 
+text(x = 1.2, y = 1.2, labels = expression(alpha),
+     cex = 1.4)
+text(x = 0, y = -0.4, labels = expression("Vw"["s"]^"T -"),
+     col = "dark grey")
+text(x = 0, y = 0.4, labels = expression("Vw"["s"]^"T +"),
+     col = "dark grey")
+text(x = 0.4, y = 0, labels = expression("Vw"["c"]^"T +"),
+     col = "dark grey")
+text(x = -0.4, y = 0, labels = expression("Vw"["c"]^"T -"),
+     col = "dark grey")
+
 # Directions of Va (heading) with respect to wind -------
 va.dir <- 90 - deg(atan2(flight.details$va_v_flt_ht,
                             flight.details$va_u_flt_ht))
@@ -289,17 +413,17 @@ lines(density.circular(x,
       rotation = "clock",
       shrink = 0.7)
 # ?lines.circular
-legend("topleft", "(e)", bty="n", cex = 1.2) 
+legend("topleft", "(f)", bty="n", cex = 1.2) 
 # legend("topright", expression(alpha), bty="n", cex = 1.2) 
-text(x = 1.2, y = 1.2, labels = expression(alpha),
+text(x = 1.2, y = 1.2, labels = expression(beta),
      cex = 1.4)
-text(x = 0, y = -0.4, labels = expression("Vw"["s"]^"-"),
+text(x = 0, y = -0.4, labels = expression("Vw"["s"]^"H -"),
      col = "dark grey")
-text(x = 0, y = 0.4, labels = expression("Vw"["s"]^"+"),
+text(x = 0, y = 0.4, labels = expression("Vw"["s"]^"H +"),
      col = "dark grey")
-text(x = 0.4, y = 0, labels = expression("Vw"["c"]^"+"),
+text(x = 0.4, y = 0, labels = expression("Vw"["c"]^"H +"),
      col = "dark grey")
-text(x = -0.4, y = 0, labels = expression("Vw"["c"]^"-"),
+text(x = -0.4, y = 0, labels = expression("Vw"["c"]^"H -"),
      col = "dark grey")
 
 
@@ -311,5 +435,7 @@ text(x = -0.4, y = 0, labels = expression("Vw"["c"]^"-"),
 # data.2 <- rvonmises(n=100, mu=circular(pi/3), kappa=3) 
 # res <- plot(data.1, stack=FALSE, col=1) 
 # points(data.2, plot.info=res, col=2)
+
+
 
 dev.off()
