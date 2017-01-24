97175618d3fa65b50cee469eecbd4525d733a2ec
thomasevans <thomas.jude.evans@gmail.com>
2017-01-24 08:48:54 +0100

    Testing for differences between species and sexes

--- summary_stats_birds.R
+++ summary_stats_birds.R
@@ -4,12 +4,15 @@
 # Load data ----
 birds <- read.csv("deployments_details.csv")
 
+str(birds)
+
 
 # Make some summaries -----
 library(plyr)
 
 
-bird.summary <- ddply(birds, .(species, sex.morph.),
+
+bird.summary <- ddply(birds, .(species, sex.new),
                       summarise,
                       mass_mean = mean(weight.kg., na.rm = TRUE),
                       mass_sd = sd(weight.kg., na.rm = TRUE),
@@ -56,12 +59,66 @@ bird.summary2 <- ddply(birds, .(species),
 
 
 # Combine above ----
-bird.summary <- bird.summary[-1,]
+# bird.summary <- bird.summary[1,]
 
-bird.summary3 <- cbind.data.frame(bird.summary2[2,1],
+bird.summary3 <- cbind.data.frame(bird.summary2[,1],
                                   sex = "All",
-                                  bird.summary2[2,2:19])
+                                  bird.summary2[,2:19])
 names(bird.summary3) <- names(bird.summary)
 birds.summary.comb <- rbind.data.frame(bird.summary3,bird.summary)
 
 write.csv(birds.summary.comb, file = "birds_summary_tab.csv")
+
+
+# Which variables (if any) are signifantly different?
+
+# murres + sex known
+sex.murres <- birds$species == "murre"
+
+# str(birds$sex.new)
+
+# mass
+
+# Sex - murres
+t.test(birds$weight.kg.[sex.murres]~birds$sex.new[sex.murres])
+
+# Sex - gulls
+t.test(birds$weight.kg.[!sex.murres]~birds$sex.new[!sex.murres])
+
+# Species
+t.test(birds$weight.kg.~birds$species)
+
+
+
+# Wing loading
+
+# Sex - murres
+t.test(birds$wing_loading..kg.m.2.[sex.murres]~birds$sex.new[sex.murres])
+
+# Sex - gulls
+t.test(birds$wing_loading..kg.m.2.[!sex.murres]~birds$sex.new[!sex.murres])
+
+# Species
+t.test(birds$wing_loading..kg.m.2.~birds$species)
+
+
+# AR
+# Sex - murres
+t.test(birds$AR[sex.murres]~birds$sex.new[sex.murres])
+
+# Sex - gulls
+t.test(birds$AR[!sex.murres]~birds$sex.new[!sex.murres])
+
+# Species
+t.test(birds$AR~birds$species)
+
+
+# Wingbeat mean
+# Sex - murres
+t.test(birds$acc.mean.f[sex.murres]~birds$sex.new[sex.murres])
+
+# Sex - gulls
+t.test(birds$acc.mean.f[!sex.murres]~birds$sex.new[!sex.murres])
+
+# Species
+t.test(birds$acc.mean.f~birds$species)
